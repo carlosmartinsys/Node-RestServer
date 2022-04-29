e7465ab26c2b8c3a56014579d80df18bd28368f0
Carlos Martin <cmartin81@gmail.com>
2022-04-29 01:01:17 -0500

    .env borrado

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -67,12 +67,7 @@ const usuariosPatch = (req, res = response) => {
 
 const usuariosDelete = async(req, res = response) => {
     const { id } = req.params;
-
-    // Lo borramos físicamente
-    // const usuario = await Usuario.findByIdAndDelete( id );
-
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false});
-
     
     res.json( usuario );
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos, validarJWT, esAdminRole, tieneRole } = require('../middlewares');
+
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
@@ -28,6 +29,9 @@ router.post( '/', [
 ], usuariosPost );
 
 router.delete( '/:id', [
+    validarJWT,
+    esAdminRole,
+    tieneRole('ADMIN_ROLE','VENTAS_ROLE','OTRO_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom( existeUsuarioPorId ),
     validarCampos
